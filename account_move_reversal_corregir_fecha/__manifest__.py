42a4b7096ef34ba6aeba3e4b2a4177df98782608
unknown <fmanime@gmail.com>
2023-04-25 12:21:28 -0600

    [ADD][reversal_corregir_fecha]

---
+++ account_move_reversal_corregir_fecha/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Igualar Fechas en Pólizas Revertidas",
+    "version": "1.0",
+    'author': "InuX",
+    'website': "www.google.com",
+    'category': '',
+    "description": """
+           Éste módulo forza la fecha en la reversa de pólizas a la fecha original del asiento a revertir.
+    """,
+    "depends": [
+        "account",
+    ],
+    "data": [
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
