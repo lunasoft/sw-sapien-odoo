0e31a0af33592b16dd66d0c489644956ba1129c4
unknown <fmanime@gmail.com>
2023-05-11 19:23:59 -0600

    [FIX][default_partner] Se corrige codigo de factura

--- l10n_mx_edi_data_default_partner/models/account_move.py
+++ l10n_mx_edi_data_default_partner/models/account_move.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models, api
-import logging
-
-_logger = logging.getLogger(__name__)
-
+from odoo import api, models, fields
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    def _get_mail_template(self):
-        """
-        :return: the correct mail template baseda en grupo
-        """
-        usuario = self.env.user
-        if usuario.has_group('account_move_corregir_outstandings.group_usar_plantilla_sw'):
-            plantilla_sw = self.env['mail.template'].search([('name', '=', 'Factura SW')])
-            if plantilla_sw:
-                return '__export__.mail_template_77_9d708080' #produccion
-                #return '__export__.mail_template_77_c2ae9992' #stag
-        else:
-            return super(AccountMove, self)._get_mail_template()
+    def _compute_l10n_mx_edi_payment_method_id(self):
+        for move in self:
+            if move.partner_id and move.partner_id.l10n_mx_edi_payment_method_id:
+                move.l10n_mx_edi_payment_method_id = move.partner_id.l10n_mx_edi_payment_method_id.id
+                print('encontre')
+            else:
+                print('no encontre')
+                super(AccountMove, self)._compute_l10n_mx_edi_payment_method_id(move)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            partner = 'partner_id' in vals and self.env['res.partner'].browse(vals['partner_id'])
+            if partner and partner.l10n_mx_edi_usage:
+                vals['l10n_mx_edi_usage'] = partner.l10n_mx_edi_usage
+        return super(AccountMove, self).create(vals_list)
