089d0729bcb3235485ec6fd33277e7941367542f
unknown <fmanime@gmail.com>
2023-05-19 16:49:05 -0600

    [IMP][default_partner] Se agrega onchange

--- l10n_mx_edi_data_default_partner/models/account_move.py
+++ l10n_mx_edi_data_default_partner/models/account_move.py
@@ -21,3 +21,8 @@ class AccountMove(models.Model):
                 move.l10n_mx_edi_payment_policy = move.l10n_mx_edi_forzar_politica
             else:
                 super(AccountMove, self)._compute_l10n_mx_edi_payment_policy()
+
+    @api.onchange("partner_id")
+    def _onchange_partner_id_default(self):
+        if self.partner_id and self.partner_id.l10n_mx_edi_usage:
+            self.l10n_mx_edi_usage = self.partner_id.l10n_mx_edi_usage
