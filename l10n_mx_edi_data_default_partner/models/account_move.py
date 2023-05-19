4e08ddd64d7827c2e9aac0bb8cd3a6dbed45eb1a
unknown <fmanime@gmail.com>
2023-05-19 16:11:20 -0600

    [OW][default_partner] Se sobreescribe default de campo odoo

--- l10n_mx_edi_data_default_partner/models/account_move.py
+++ l10n_mx_edi_data_default_partner/models/account_move.py
@@ -6,6 +6,7 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     l10n_mx_edi_forzar_politica = fields.Selection(string='Forzar política de pago', selection=[('PPD', 'PPD'), ('PUE', 'PUE')])
+    l10n_mx_edi_usage = fields.Selection(selection_add=[], default=lambda self: self.partner_id.l10n_mx_edi_usage)
 
     def _compute_l10n_mx_edi_payment_method_id(self):
         for move in self:
@@ -13,14 +14,6 @@ class AccountMove(models.Model):
                 move.l10n_mx_edi_payment_method_id = move.partner_id.l10n_mx_edi_payment_method_id.id
             else:
                 super(AccountMove, self)._compute_l10n_mx_edi_payment_method_id()
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            partner = 'partner_id' in vals and self.env['res.partner'].browse(vals['partner_id'])
-            if partner and partner.l10n_mx_edi_usage:
-                vals['l10n_mx_edi_usage'] = partner.l10n_mx_edi_usage
-        return super(AccountMove, self).create(vals_list)
 
     def _compute_l10n_mx_edi_payment_policy(self):
         for move in self:
